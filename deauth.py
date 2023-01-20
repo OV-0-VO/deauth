fd73c066b75b287bbabe8f2cd83a7f65fa799575
OV-0-VO <84079050+OV-0-VO@users.noreply.github.com>
2023-01-20 16:09:44 +0900

    init
    deauth-attack

---
+++ deauth.py
@@ -0,0 +1,14 @@
+from scapy.all import *
+import sys
+
+def deauth():
+  print("deauth")
+  return
+  
+
+if __name__ == "__main__":
+  for arg in sys.argv:
+    print(arg)
+    
+    deauth()
+    
